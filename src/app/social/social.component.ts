a302fad2c83a02486ba683827140570a24f3b13a
kiran <kiran@vasanti.org>
2019-06-06 12:03:01 -0400

    Committed

---
+++ src/app/social/social.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ns-social',
+  templateUrl: './social.component.html',
+  styleUrls: ['./social.component.css'],
+  moduleId: module.id,
+})
+export class SocialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
